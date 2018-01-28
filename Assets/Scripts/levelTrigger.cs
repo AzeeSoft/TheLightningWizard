b88336f445ee67a4db8b030ad57a00646f6f9582
MelisaJ <MAJohnson4683@muleriders.saumag.edu>
2018-01-28 07:29:17 -0600

    TRIGGERS!!!!

---
+++ Assets/Scripts/levelTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class levelTrigger : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider col){
+        if (col.tag=="Player"){
+            SceneManager.LoadScene("LevelTwo", LoadSceneMode.Single);//load scene level
+        }
+    }
+}
