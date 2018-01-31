b1cddb74e9365dcb7bd9ae2c26f29edf7023a625
Azee <aziztitu1996@gmail.com>
2018-01-31 17:32:32 -0600

    Added screen transitions.

---
+++ Assets/Scripts/Misc/ScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+
+    public Animator screenTransitionAnim;
+
+	// Use this for initialization
+	void Start () {
+        open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void open()
+    {
+        screenTransitionAnim.SetBool("Open", true);
+    }
+
+    void close()
+    {
+        screenTransitionAnim.SetBool("Open", false);
+    }
+}
